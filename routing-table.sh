63f331ac1fce3d08a38e29c325c7c40032ebc4d3
vedant-204 <katyayana2003@gmail.com>
2023-04-05 14:46:27 +0530

    routing table inclusion

---
+++ routing-table.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for instance in worker-0 worker-1; do
+  gcloud compute instances describe ${instance} \
+    --format 'value[separator=" "](networkInterfaces[0].networkIP,metadata.items[0].value)'
+done
+
+for i in 0 1; do
+  gcloud compute routes create kubernetes-route-10-200-${i}-0-24 \
+    --network kubernetes-the-hard-way \
+    --next-hop-address 10.240.0.2${i} \
+    --destination-range 10.200.${i}.0/24
+done
